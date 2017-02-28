f0262768bd3eb004f0c0c66372d47ef6527a24bb
philcms1 <phil@soaarchitects.com>
2017-02-28 12:56:55 -0700

    Renamed injected providers

--- dist/logging.factory.js
+++ dist/logging.factory.js
@@ -1,8 +1,8 @@
  (function() {
      'use strict';
-     angular.module('appLogging').factory('$log', ['appContext', 'appConfig', logFactory]);
+     angular.module('appLogging').factory('$log', ['AppContext', 'AppConfig', logFactory]);
 
-     function logFactory(appContext, appConfig) {
+     function logFactory(AppContext, AppConfig) {
          var
              soaaLogger, consoleAppender,
              ajaxAppender, jsonLayout;
@@ -13,20 +13,20 @@
          consoleAppender = new log4javascript.BrowserConsoleAppender();
          soaaLogger.addAppender(consoleAppender);
          // batch to backend server
-         ajaxAppender = new log4javascript.AjaxAppender(appConfig.logUrl);
+         ajaxAppender = new log4javascript.AjaxAppender(AppConfig.logUrl);
          jsonLayout = new log4javascript.JsonLayout();
          // Adding custom fields
-         jsonLayout.setCustomField('sourceApp', appConfig.serviceName);
+         jsonLayout.setCustomField('sourceApp', AppConfig.serviceName);
          jsonLayout.setCustomField('browserUserAgent', navigator.userAgent);
-         jsonLayout.setCustomField('requestId', appContext.requestId);
+         jsonLayout.setCustomField('requestId', AppContext.requestId);
          jsonLayout.setCustomField('focusPersonMmi', SapphireContext.mmi);
          ajaxAppender.setLayout(jsonLayout);
          ajaxAppender.setSendAllOnUnload(true);
-         ajaxAppender.setBatchSize(appConfig.logBatchSize);
+         ajaxAppender.setBatchSize(AppConfig.logBatchSize);
          ajaxAppender.addHeader('Content-Type', 'application/json');
          soaaLogger.addAppender(ajaxAppender);
          // Set default log level to INFO -- lower levels won't be logged
-         log4javascript.getLogger('soaa').setLevel(log4javascript.Level[appConfig.loggingLevel]);
+         log4javascript.getLogger('soaa').setLevel(log4javascript.Level[AppConfig.loggingLevel]);
 
          return soaaLogger;
      }
